c70d3968aadc8b8d051145a405abb9c948f17f7b
louisme.2014@gmail.com <louisme.2014@gmail.com>
2025-08-11 15:05:38 +0200

    Add missing includes in Texture2D & MeshData

--- src/engine/renderer/texture/texture2d.h
+++ src/engine/renderer/texture/texture2d.h
@@ -7,6 +7,7 @@
 #include <mutex>
 #include <string>
 #include <unordered_map>
+#include <array>
 
 // --------------------------------------------------------------------------
 //  2D texture wrapper
